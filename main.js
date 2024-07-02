f71b7042ececa705c0e72d5ad49eff4162af2bd3
geonhee <ghkorean@naver.com>
2024-07-02 16:29:23 +0900

    마지막

--- main.js
+++ main.js
@@ -9,6 +9,7 @@
 //유저가 이미 입력한 숫자를 또 입력하면, 알려준다, 기회를 깍지 않는다.
 //유저가 숫자 입력후 재입력할때 알아서 숫자가 지워지게 하는 것.
 //유저가 숫자를 맞추면 go 버튼이 없어지게 한다.
+//기회를 다 날리면 gameover가 된다.
 
 let computerNum="0"
 let playButton = document.getElementById("play-button")
@@ -40,7 +41,7 @@ function play(){
         resultArea.textContent="1과 100사이 숫자를 입력해주세요."
 return;
     }
-numHistory.textContent=`[${history}]`
+
 
 if(history.includes(userValue)){
     resultArea.textContent="이미 입력한 숫자입니다. 다른 숫자를 입력해주세요."
@@ -50,32 +51,46 @@ return
 
 chances--;
 chanceArea.textContent=`남은기회 :${chances}번`
-console.log("chance",chances)
+numHistory.textContent=`[${history}]`
 
-if(userValue<computerNum){
+if(chances <= 0 && userValue != computerNum){
+resultArea.textContent="Game Over"
+gameOver=true
+}
+
+else if(userValue<computerNum){
     resultArea.textContent="위로!"
 }else if(userValue>computerNum){
     resultArea.textContent="아래로!"
 }else{
     resultArea.textContent="정답!"
-    gameOver = true
+    gameOver=true
 }
 
 history.push(userValue)
 console.log(history)
 
+
+
 if(chances <1){
     gameOver=true
+    
 }
 if(gameOver==true){
     playButton.disabled = true
-    resultArea.textContent="Game Over"
+    
 }
 }
 function reset(){
     userInput.value=""
     pickRandomNum()
-    resultArea.textContent="결과값이 여기 나옵니다"
+    resultArea.textContent="마시기 싫으면 맞춰라"
+    playButton.disabled = false
+    gameOver = false
+    chances = 3
+    chanceArea.textContent=`남은기회 :${chances}번`
+    history=[]
+    numHistory.textContent=`[${history}]`
 }
 
 pickRandomNum()
